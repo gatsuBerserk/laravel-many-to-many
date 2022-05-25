2d0bd3a6c0f28333b280043100eeb7f5f836f072
gatsuBerserk <berserkdev@outlook.it>
2022-05-25 20:02:59 +0200

    ratation one to many user-post

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -7,11 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable=[
+        "user_id",
         "title", 
         "author", 
         "content", 
         "image_url"
-    ]; 
+    ];
+    
+    public function utent(){
+        return $this->belongsTo("App\User");
+    }
 
     public function categories(){
         return $this->belongsToMany("App\Model\Category");
--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Model\Post;
+use App\User;
+
 
 class PostTableSeeder extends Seeder
 {
@@ -13,8 +15,11 @@ class PostTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $user_ids = User::pluck('id')->toArray();
+        
         for ( $i = 0; $i < 50 ; $i++) { 
             $newPost = new Post();
+            $newPost->user_id = $faker->randomElement($user_ids);
             $newPost->title = $faker->words(3, true);
             $newPost->author = $faker->name();
             $newPost->content = $faker->paragraph(3, false);
